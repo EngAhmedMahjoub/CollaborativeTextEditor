9f3079b5e1743a9010857f781dce2e9058fc82f8
Ahmed-RSE <81463607+Ahmed-RSE@users.noreply.github.com>
2023-10-05 08:54:41 +0300

    TC.#1 .add signal r to client site code

--- CollaborativeTextEditor/wwwroot/js/site.js
+++ CollaborativeTextEditor/wwwroot/js/site.js
@@ -1,4 +1,26 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿"use strict";
 
-// Write your JavaScript code.
+var connection = new signalR.HubConnectionBuilder().withUrl("/texthub").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveText", function (text) {
+    var li = document.createElement("li");
+    li.textContent = text;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var text = document.getElementById("messageInput").value;
+    connection.invoke("BroadcastText", text).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
